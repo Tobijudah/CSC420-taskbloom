19f6138c194206e1865f383a9379b89fb501d580
Oluwatobiju Judah <tobijudah@gmail.com>
2024-06-19 22:40:22 +0100

    chore: Update API URL to include "/api/"

--- src/redux/slices/apiSlice.js
+++ src/redux/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // const API_URL = import.meta.env.VITE_APP_BASE_URL + "/api";
-const API_URL = "http://127.0.0.1:8000/";
+const API_URL = "http://127.0.0.1:8000/api/";
 
 const baseQuery = fetchBaseQuery({ baseUrl: API_URL });
 
