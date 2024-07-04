fe15469b8301ffee28ab1516ec350db7bbd68e73
Oluwatobiju Judah <tobijudah@gmail.com>
2024-07-04 01:44:53 +0100

    chore: Update Login page error handling to display detailed error message

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
       dispatch(setCredentials(res));
       navigate("/");
     } catch (err) {
-      toast.error(err?.data?.message || err.error);
+      toast.error(err?.data?.message || err.error || err?.data?.detail);
     }
   };
 
